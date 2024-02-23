8f46d634a8b130535bb5d66bd781e2368d9eefdc
Konstantin <iksiks30@gmail.com>
2024-02-23 16:11:03 +0300

    Добавьте файлы проекта.

---
+++ HandmadeСosmetics/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HandmadeСosmetics
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ HandmadeСosmetics/Command/LambdaCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace HandmadeСosmetics.Command
+{
+    internal class LambdaCommand : CommandBase
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
