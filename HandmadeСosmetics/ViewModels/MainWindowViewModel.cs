174516ba109a904e490f7db123d84e0d87153433
Konstantin <iksiks30@gmail.com>
2024-03-03 02:05:16 +0300

    Изменил доступность записи у страниц. Создал команду открытия страниц

--- HandmadeСosmetics/ViewModels/MainWindowViewModel.cs
+++ HandmadeСosmetics/ViewModels/MainWindowViewModel.cs
@@ -1,48 +1,88 @@
-﻿using HandmadeСosmetics.Views.Pages;
+﻿using HandmadeСosmetics.Command;
+using HandmadeСosmetics.ViewModel;
+using HandmadeСosmetics.Views.Pages;
+using System.ComponentModel;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HandmadeСosmetics.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModelBase
     {
-        private Page currentContent;
+        #region Pages
+
+        private Page currentPage;
         private Page pageRecipes;
         private Page pageCatalog;
         private Page pageClients;
         private Page pageComponents;
 
-        public Page CurrentContent
+        public Page CurrentPage
         {
-            get { return currentContent; }
-            set { currentContent = value; }
+            get { return currentPage; }
+            set { Set(ref currentPage, value); }
         }
 
         public Page PageCatalog
         {
             get { return pageCatalog; }
-            set { pageCatalog = value; }
+            private set { pageCatalog = value; }
         }
 
         public Page PageClients
         {
             get { return pageClients; }
-            set { pageClients = value; }
+            private set { pageClients = value; }
         }
 
         public Page PageComponents
         {
             get { return pageComponents; }
-            set { pageComponents = value; }
+            private set { pageComponents = value; }
         }
 
         public Page PageRecipes
         {
             get { return pageRecipes; }
-            set { pageRecipes = value; }
+            private set { pageRecipes = value; }
         }
+
+        #endregion Pages
 
         public MainWindowViewModel()
         {
+            pageCatalog = new PageCatalog();
+            pageComponents = new PageComponents();
+            pageRecipes = new PageRecipes();
+            pageClients = new PageClients();
+            SetCurrentPageCommand = new LambdaCommand(OnSetCurrentPageCommandExecuted);
+        }
+
+        public ICommand SetCurrentPageCommand { get; }
+
+        private void OnSetCurrentPageCommandExecuted(object p)
+        {
+            var b = p as Button;
+            switch (b.Content)
+            {
+                case "КАТАЛОГ":
+                    CurrentPage = PageCatalog;
+                    break;
+
+                case "КЛИЕНТЫ":
+                    CurrentPage = PageClients;
+                    break;
+
+                case "РЕЦЕПТЫ":
+                    CurrentPage = PageRecipes;
+                    break;
+
+                case "КОМПОНЕНТЫ":
+                    CurrentPage = PageComponents;
+                    break;
+
+                default: break;
+            }
         }
     }
 }
